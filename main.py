a372152c2537a2c1f5ad338952e2597417662438
vagutierrezv@uqvirtual.edu.co <vagutierrezv@uqvirtual.edu.co>
2022-06-05 15:46:13 -0500

    Ajuste Info Api Clima

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import requests
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+url = "https://www.datos.gov.co/resource/ka9f-zy7y.json"
 
+headers = {
+    "Accept": "application/json",
+    "Accept-Encoding": "gzip"
+}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+response = requests.get(url, headers=headers)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(response.text)
