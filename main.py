f8992684dfeecdb6caae8d12fc5823f271748975
Alejandro Castaño <castanoalejo4@gmail.com>
2022-06-05 16:27:28 -0500

    Prueba conexion repositorio

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 # Api del Estaciones Meteorológicas e Hidrológicas de Colombia
 # Implementacion Api
+# Prueba conexion repositorio
 url = "https://www.datos.gov.co/resource/ka9f-zy7y.json"
 
 headers = {
