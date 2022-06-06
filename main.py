7a539cf5a407c81ba0d89f64e0494f6d5ac784d0
vagutierrezv@uqvirtual.edu.co <vagutierrezv@uqvirtual.edu.co>
2022-06-06 14:27:44 -0500

    Prueba

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 # Api del Estaciones Meteorológicas e Hidrológicas de Colombia
+# prueba
 # Implementacion Api
 # Prueba conexion repositorio
 url = "https://www.datos.gov.co/resource/ka9f-zy7y.json"
